2b2c18e5eafae8349cb6607cb47f77e3e524b5d1
Davi Marriel <davi.marriel@42we.tech>
2024-02-27 10:05:50 -0300

    Compiling with the two submodules together

---
+++ Firmware/Core/Inc/Services/EspFlasherService.h
@@ -0,0 +1,13 @@
+/**
+ * @file EspFlasherService.h
+ * @author Davi Marriel (davi.marriel@42we.tech)
+ * @brief 
+ * @version 0.1
+ * @date 27-02-2024
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#pragma once
+
